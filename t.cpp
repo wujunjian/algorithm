d30057adda0fd46b7d1088bed617c4498da96e3c
武俊健 <56492724@qq.com>
2018-07-25 15:55:36 +0800

    Add files via upload

---
+++ t.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+//test with -fno-elide-constructors
+
+using namespace std;
+class A
+{
+public:
+    A():m_ptr(new int(0)){cout << "construct" << endl;}
+    A(const A& a):m_ptr(new int(*a.m_ptr)) //深拷贝的拷贝构造函数
+    {
+        cout << "copy construct" << endl;
+    }
+    ~A(){ delete m_ptr;}
+private:
+    int* m_ptr;
+};
+
+
+A GetA()
+{
+	return A();
+}
+
+int main() {
+    A a = GetA();
+    return 0;
+}
