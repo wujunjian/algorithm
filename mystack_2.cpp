db7906bd7b9b7f2064b8661300b13d7b85f88971
武俊健 <56492724@qq.com>
2018-07-26 23:21:53 +0800

    cmake mystack2

---
+++ mystack_2.cpp
@@ -0,0 +1,70 @@
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+
+
+using namespace std;
+
+class Mystack {
+private:
+    vector<int> stackData;
+    vector<int> stackMin;
+
+public:
+    Mystack() {
+        
+    }
+
+    Mystack& push(int newNum) {
+        if(stackMin.empty() || stackMin.back() >= newNum)
+        {
+            stackMin.push_back(newNum);
+        } 
+        stackData.push_back(newNum);
+        return *this;
+    }
+    int pop() {
+        if(stackData.empty()) {
+            throw std::runtime_error("the stack is empty");
+        } 
+
+        if(stackData.back() == stackMin.back())
+        {
+            stackMin.pop_back();
+        }
+        int value = stackData.back();
+        stackData.pop_back();
+        return value;
+    }
+    int getmin() {
+         return stackMin.back();
+    }
+
+	int length() {
+		return stackData.size();
+	}
+};
+
+
+int main(){
+
+    class Mystack s;
+    s.push(3).push(4).push(5).push(1).push(2).push(1);
+    cout << "s.push(3).push(4).push(5).push(1).push(2).push(1);" << endl;
+    cout <<  "min:" << s.getmin() << endl;
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+    cout << s.getmin() << "stackData size:" << s.length() << endl;
+
+
+	s.push(100);
+	s.push(99);
+	s.push(0);
+	cout << s.getmin() << "stackData size:" << s.length() << endl;
+
+	cin.get();
+    return 0;
+}
+
+
