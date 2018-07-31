6fe02fb7d5630d5bb836cc4d6fd61f7ac486e263
wujunjian <56492724@qq.com>
2018-07-31 20:06:07 +0800

    reversenode

---
+++ reversenode.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+
+using namespace std;
+
+class Node {
+public:
+	int m_data;
+	Node(int data) :m_data(data) { next = NULL; }
+	Node * next;
+};
+
+Node *reversenode(Node * node)
+{
+	if (node->next == NULL)
+	{
+		return node;
+	}
+	else {
+		reversenode(node->next)->next = node;
+	}
+	return node;
+}
+
+
+
+int main()
+{
+	Node a(0);
+	Node b(1);  a.next = &b;
+	Node c(2);  b.next = &c;
+	Node d(3);  c.next = &d;
+	Node e(4);  d.next = &e;
+	Node f(5);  e.next = &f;
+	Node g(6);  f.next = &g;
+
+	reversenode(&a)->next = NULL;
+	auto begin = &g;
+	for (; begin != NULL; begin = begin->next)
+	{
+		cout << begin->m_data << endl;
+	}
+
+
+}
